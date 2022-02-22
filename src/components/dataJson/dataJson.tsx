03b0af07ea04235d68188ce873becc7771c45753
AleksRomanov <kosmonavt-vavilon@mail.ru>
2022-02-22 19:37:48 +0700

    Work on progress +

--- src/components/dataJson/dataJson.tsx
+++ src/components/dataJson/dataJson.tsx
@@ -5,18 +5,18 @@ import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 import TreeItem from '@mui/lab/TreeItem';
 import JsonData from '../../utils/examp.json';
 
-
 function DataJson() {
-    const data = JsonData[0].contents[0].contents;
-    // const data = Object.entries(JsonData)[0];
-    // const data2 = data[1].contents;
+    // const data = JsonData[0].contents[0].contents;
+    const data = Object.entries(JsonData)[0];
+    const bil = data[1].contents;
     // console.log(data);
     // if (data) {
-    const DisplayData = data.map(
+    const DisplayData = bil.map(
         (info: any, i: any) => {
             // console.log(info)
             return (
                 <tr key={i}>
+                    <th>{info.type}</th>
                     <th>{info.name}</th>
                     <th>{info.size}</th>
                 </tr>
@@ -29,34 +29,26 @@ function DataJson() {
     // console.log(JsonData[0].contents[0].contents);
     // console.log(JsonData[0].contents[0].contents);
     return (
-        <TreeView
-            aria-label="file system navigator"
-            defaultCollapseIcon={<ExpandMoreIcon/>}
-            defaultExpandIcon={<ChevronRightIcon/>}
-            sx={{height: 240, flexGrow: 1, maxWidth: 400, overflowY: 'auto'}}
-        >
-            <TreeItem nodeId="1" label="Applications">
-                <TreeItem nodeId="2" label="Calendar"/>
-            </TreeItem>
-            <TreeItem nodeId="5" label="Documents">
-                <TreeItem nodeId="10" label="OSS"/>
-                <TreeItem nodeId="6" label="MUI">
-                    <TreeItem nodeId="8" label="index.js"/>
+        <table>
+            <thead>
+            <tr>
+                <td>{DisplayData.type}</td>
+                <td>File name</td>
+                <td>size</td>
+            </tr>
+            </thead>
+            <tbody>
+            {/*{DisplayData}*/}
+            <TreeView aria-label="file system navigator" defaultCollapseIcon={<ExpandMoreIcon/>} defaultExpandIcon={<ChevronRightIcon/>} sx={{border: '1px solid red', height: 600, flexGrow: 1, width: 200, overflowY: 'auto'}}>
+                <TreeItem nodeId="1" label={DisplayData}>
+                    <TreeItem nodeId="2" label={DisplayData}>
+                        <TreeItem nodeId="3" label={DisplayData}/>
+                    </TreeItem>
                 </TreeItem>
-            </TreeItem>
-            {DisplayData}
-        </TreeView>
-        // <table>
-        //     <thead>
-        //     <tr>
-        //         <th>File name</th>
-        //         <th>size</th>
-        //     </tr>
-        //     </thead>
-        //     <tbody>
-        //     {DisplayData}
-        //     </tbody>
-        // </table>
+            </TreeView>
+            </tbody>
+        </table>
+
     )
 }
 
